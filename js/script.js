d2c4815de9389cdc83e4ae1a2cdec04407408db0
Muhammad Zidan <mzidan0116@gmail.com>
2024-03-18 22:11:41 +0700

    modif commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-// TOGGLE CLASS ACTIVATE
 const navbarNav = document.querySelector('.navbar-nav');
 
-//KETIKA HAMBURGER DI KLIK
 document.querySelector('#hamburger-menu').onclick=()=>{
     navbarNav.classList.toggle('active')
 };
 
-
-//KLIK DILUAR SIDEBAR UNTUK MENGHILANGKAN NAV
 const hamburger = document.querySelector('#hamburger-menu');
 document.addEventListener('click', function(e){
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
